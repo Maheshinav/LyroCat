9d90585561b52b49de32779e0a98ea789e3fc648
MaheshiNav <maheshinawarathna29@gmail.com>
2023-08-25 18:11:53 -0700

    Add off canvas menu for My items

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
-import {Link} from 'react-router-dom';
+import { useState, useRef } from "react";
+import { Offcanvas } from "bootstrap";
+
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsFillCircleFill } from "react-icons/bs";
@@ -11,7 +13,17 @@ const NavBar = ({ cartCount }: { cartCount: number }) => {
 	const toggleMenu = (): void => {
 		setIsMenuOpen(!isMenuOpen);
 	};
-	const Logo= "https://res.cloudinary.com/sudeshmaldivesbook/image/upload/v1692995251/d221d82875e0490ca474a9d13884e0ab.png"
+
+	const handleOffcanvasToggle = () => {
+		if (offcanvasRef.current) {
+			const bsOffcanvas = new Offcanvas(offcanvasRef.current);
+			bsOffcanvas.toggle();
+		}
+	};
+	const offcanvasRef = useRef<HTMLDivElement | null>(null);
+
+	const Logo =
+		"https://res.cloudinary.com/sudeshmaldivesbook/image/upload/v1692995251/d221d82875e0490ca474a9d13884e0ab.png";
 	return (
 		<div>
 			{isMenuOpen && <SideMenu />}
@@ -35,16 +47,17 @@ const NavBar = ({ cartCount }: { cartCount: number }) => {
 								color="#ef9b0f"
 								size={25}
 								className="me-2"
+								ddata-bs-toggle="offcanvas"
+								data-bs-target="#offcanvasWithBothOptions"
+								title="View your shopping cart"
+								onClick={handleOffcanvasToggle}
 							/>
-							<div style={{ position: "relative" }}>
+							<div className="position-relative">
 								<BsFillCircleFill color="#ef9b0f" size={20} />
 								{cartCount > 0 && (
 									<span
+										className="position-absolute top-50 start-50 translate-middle"
 										style={{
-											position: "absolute",
-											top: "50%",
-											left: "50%",
-											transform: "translate(-50%, -50%)",
 											color: "white",
 											fontSize: "12px",
 										}}
@@ -68,11 +81,37 @@ const NavBar = ({ cartCount }: { cartCount: number }) => {
 					</div>
 				</div>
 			</div>
-			<Link to ="/"> 
-			<div className="d-flex justify-content-center align-items-center">
-    <img src={Logo} alt="Logo Description" className="img-fluid p-2" />
-</div>
-</Link>
+			<Link to="/">
+				<div className="d-flex justify-content-center align-items-center">
+					<img src={Logo} alt="Logo Description" className="img-fluid p-2" />
+				</div>
+			</Link>
+
+			<div
+				ref={offcanvasRef}
+				className="offcanvas offcanvas-start"
+				data-bs-scroll="true"
+				tabIndex={-1}
+				id="offcanvasWithBothOptions"
+				aria-labelledby="offcanvasWithBothOptionsLabel"
+			>
+				<div className="offcanvas-header">
+					<h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+						Your Items
+					</h5>
+					<button
+						type="button"
+						className="btn-close"
+						data-bs-dismiss="offcanvas"
+						aria-label="Close"
+					></button>
+				</div>
+				<div className="offcanvas-body">
+					<p>
+					</p>
+				</div>
+			</div>
+
 			<hr className="hr" />
 		</div>
 	);
